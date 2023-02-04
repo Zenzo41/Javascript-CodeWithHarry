36837ac2eaefb242e49e0bfed06a4a7db22c8d41
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-02-04 09:36:10 +0545

    Primitive and Non primitive data types

--- 04PrimitiveDataTypesandObjects.js
+++ 04PrimitiveDataTypesandObjects.js
@@ -1,1 +1,33 @@
-// NNSSBBU (null ,number , symbol , string, bigint, boolean , undefined)
+// NNSSBBU (null ,number , symbol , string, bigint, boolean , undefined) and Non primitive - Object
+let a = null
+let b = 3434
+let c = Symbol("I am a nice symbol")
+let d = "Zen"
+let e = BigInt(232424235235) + BigInt(20)
+let f = true //can be false too
+let g = undefined
+let h
+
+console.log(a,b,c,d,e,f,g)
+console.log(typeof(c))
+console.log(typeof(h)) // default
+
+// Objects
+const items = {
+    name : "Sonam",
+    value : 3454,
+    is_true : false,
+    lrg_val : 13131,
+    has_wife : null,
+    symbolval : Symbol("Noob"),
+    money_wasted : undefined
+
+}
+console.log("Name: "+ items.name,
+            "Value: "+ items.value,
+            "Male: " +items.is_true,
+            "Phone Number: "+items.lrg_val,
+            "Has Wife: "+items.has_wife,
+            items.symbolval,
+            "Money Wasted: "+items.money_wasted
+            )
