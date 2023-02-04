e0a45274cce8860672d033673f1629d1238ee04a
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-02-04 09:09:35 +0545

    Const , Let and Var

---
+++ 03Const-Let-Var.js
@@ -0,0 +1,20 @@
+var q = "here"
+var q = "thicc"
+console.log(q)  //Try to use const more and then let . Don't use var
+
+let b = "Here"
+const author = "Zen" //can't do this to constant varibales (let author = "hero")
+b = 45 // can
+//can't let b = "harry"
+
+{
+    let b = "this"  //can be done
+    console.log(b)
+}
+console.log(b)
+
+let c = null
+console.log(c)
+
+const value = 0  //Constant must be initialized 
+
