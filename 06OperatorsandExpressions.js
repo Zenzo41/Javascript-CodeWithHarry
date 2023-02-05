cc7d848acb4dd5964282a560e62d6c116cfbfd0a
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-02-05 19:35:52 +0545

    Operators as well as comments

--- 06OperatorsandExpressions.js
+++ 06OperatorsandExpressions.js
@@ -1,6 +1,7 @@
-console.log("Basic Operations:")
+
 let a = 20;
 let b = 30;
+console.log("Arithmetical Operators: ")
 console.log("a + b: ",a + b);
 console.log("a - b: ",a - b);
 console.log("a * b: ",a * b);
@@ -11,3 +12,40 @@ console.log("a ++ :",a ++);
 console.log("a -- :",a --);
 console.log("++ a :",++ a );
 console.log("-- a :",-- a );
+
+console.log("Comparison Operators: ")
+console.log("a == b : ", a == b)
+console.log("a != b : ", a != b)
+console.log("a === b : ", a === b)
+console.log("a !== b : ", a !== b)
+console.log("a > b : ", a > b)
+console.log("a < b : ", a < b)
+console.log("a >= b : ", a >= b)
+console.log("a <= b : ", a <= b)
+
+console.log("Logical Operators: ")
+console.log("a<b && a == 5 : ",a<b && a == 5)
+console.log("a>b && a %5 == 0 : ",a<b || a == 5)
+console.log("! false : ",!false)
+
+// Single Line Comment in JavaScript
+/* Multi Line Comment in JavaScript     
+    async function getData() {
+  return Promise.resolve('data');
+}
+
+async function getMoreData(data) {
+  return Promise.resolve(data + 'more data');
+}
+
+async function getAll() {
+  const data = await getData();
+  const moreData = await getMoreData(data);
+  return `All the data: ${data}, ${moreData}`;
+}
+
+getAll().then((all) => {
+  console.log('all the data')
+})
+
+*/
